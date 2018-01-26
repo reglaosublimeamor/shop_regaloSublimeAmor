5049b7a14098c0bd03eceb18b220e513e4565e59
Cristian Franco <cfrancobedoya@gmail.com>
2018-01-26 17:42:03 -0500

    CONFIGURANDO FIREBASE

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  port: process.env.PORT || 3001,
-  owner: "Alejandra Bertón Manguta",
-  developer: "Cristian Franco Bedoya"
-}
--- index.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app')
-const config = require('./config')
-
-app.listen(config.port, function () {
-  // Puerto en el que está corriendo la aplicación
-  console.log(`Aplicación corriendo en http://localhost:${config.port}`)
-  console.log(`Propiedad de ${config.owner}`)
-  console.log(`Desarrollado por ${config.developer}`)
-})
